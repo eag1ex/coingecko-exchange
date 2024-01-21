ef9b20dd06701e6f487ae099b1055b5ea5b72683
eag1ex <eaglex@eaglex.net>
2024-01-21 22:08:37 +0700

    Updates

---
+++ precache-manifest.7fddb027139101d8f8507928456b68e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c320330a6494e6571cd3298b34b05ab2",
+    "url": "/coingecko-exchange/app/exchanges/index.html"
+  },
+  {
+    "revision": "6aeea2f1fe6a2f1d14a7",
+    "url": "/coingecko-exchange/app/exchanges/static/css/main.d96985db.chunk.css"
+  },
+  {
+    "revision": "cb7fe46aedffeb7d8241",
+    "url": "/coingecko-exchange/app/exchanges/static/js/2.77dd0515.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/coingecko-exchange/app/exchanges/static/js/2.77dd0515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aeea2f1fe6a2f1d14a7",
+    "url": "/coingecko-exchange/app/exchanges/static/js/main.78a80b8d.chunk.js"
+  },
+  {
+    "revision": "e985c3ebbbdcfc1a0411",
+    "url": "/coingecko-exchange/app/exchanges/static/js/runtime-main.803818c6.js"
+  },
+  {
+    "revision": "2522dee87b937e003442a4b4e9bcf5f8",
+    "url": "/coingecko-exchange/app/exchanges/static/media/slack_icon.2522dee8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.68b819eb6ce070022e1ce7d3d53ad964.js"
+  "/coingecko-exchange/app/exchanges/precache-manifest.7fddb027139101d8f8507928456b68e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coingecko-exchange/app/exchanges/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
